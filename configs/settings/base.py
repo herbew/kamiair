fa2b9ad71089cd3ae63bfabf170181a94c997c4c
herbew <herbew@h-13ITL6>
2023-12-20 23:09:54 +0700

    add allauth.account.middleware.AccountMiddleware to settings.MIDDLEWARE

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -157,6 +157,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'allauth.account.middleware.AccountMiddleware' # Only django-allauth v0.56+
 ]
 
 # STATIC
