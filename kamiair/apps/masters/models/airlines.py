b6c11eac95c79fec2ba5d8e3df18fb6d98257d96
herbew <herbew@h-13ITL6>
2023-12-21 05:45:28 +0700

    used TimeStampedModel

--- kamiair/apps/masters/models/airlines.py
+++ kamiair/apps/masters/models/airlines.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals, absolute_import
 import logging
 
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 from django.utils.translation import gettext_lazy as _
 
 log = logging.getLogger(__name__)
 
-class Airlines(models.Model):
+class Airlines(TimeStampedModel):
     """
     Description: 
         An airline is a company that provides air transport services for 
