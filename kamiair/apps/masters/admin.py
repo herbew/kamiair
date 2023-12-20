9c43e5b42e84451149960a60cd3474823d74d87a
herbew <herbew@h-13ITL6>
2023-12-21 04:57:00 +0700

    Airlines path

--- kamiair/apps/masters/admin.py
+++ kamiair/apps/masters/admin.py
@@ -2,6 +2,6 @@ from __future__ import unicode_literals, absolute_import
 
 from django.contrib import admin
 
-from kamiair.apps.models.airlines import Airlines
+from kamiair.apps.masters.models.airlines import Airlines
 
 admin.site.register(Airlines)
