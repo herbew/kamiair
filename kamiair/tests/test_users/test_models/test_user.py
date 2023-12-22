dc1cc46f4e28442340f687a48b53ed7a1e8be8c5
herbew <herbew@h-13ITL6>
2023-12-23 06:44:58 +0700

    test get user

--- kamiair/tests/test_users/test_models/test_user.py
+++ kamiair/tests/test_users/test_models/test_user.py
@@ -6,5 +6,9 @@ from kamiair.apps.users.models import User
 class UserTestCase(TestCase):
     def setUp(self):
         User.objects.create(username="herbew")
+        
+    def test(self):
+        user = Use.objects.get("herbew")
+        self.assertEqual(user.username,'herbew')
     
     
