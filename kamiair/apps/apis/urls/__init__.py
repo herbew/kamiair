f5357442f1341e5e0924b0ec469acff10d2b8760
herbew <herbew@h-13ITL6>
2023-12-25 21:24:51 +0700

    import path url

--- kamiair/apps/apis/urls/__init__.py
+++ kamiair/apps/apis/urls/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from kamiair.apps.urls.assumptions import urlpatterns as assumptions_urlpatterns
+from kamiair.apps.apis.urls.assumptions import urlpatterns as assumptions_urlpatterns
 
 app_name = "apis"
 
